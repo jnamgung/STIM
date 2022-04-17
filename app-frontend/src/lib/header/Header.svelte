dcd49ea0f22d724fdfa12c529a7968676018a1db
Nithin Ravi <nravi@andrew.cmu.edu>
2022-04-16 19:25:58 -0600

    Completed Severity levels UI

--- app-frontend/src/lib/header/Header.svelte
+++ app-frontend/src/lib/header/Header.svelte
@@ -16,11 +16,11 @@
 		</svg>
 		<ul>
 			<li class:active={$page.url.pathname === '/'}><a sveltekit:prefetch href="/">Home</a></li>
-			<li class:active={$page.url.pathname === '/about'}>
-				<a sveltekit:prefetch href="/about">About</a>
+			<li class:active={$page.url.pathname === '/severity_levels'}>
+				<a sveltekit:prefetch href="/severity_levels">Severity Level</a>
 			</li>
-			<li class:active={$page.url.pathname === '/todos'}>
-				<a sveltekit:prefetch href="/todos">Todos</a>
+			<li class:active={$page.url.pathname === '/another_page'}>
+				<a sveltekit:prefetch href="/another_page">Another Page</a>
 			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
--- app-frontend/src/routes/severity_levels.svelte
+++ app-frontend/src/routes/severity_levels.svelte
@@ -2,17 +2,25 @@
 	import { Card, CardText, CardActions, Button, MaterialApp } from 'svelte-materialify';
 </script>
   
-
 <MaterialApp>
 	<Card outlined style="max-width:2000px;">
 		<div class="pl-4 pr-4 pt-3">
-		  <span class="text-h5 mb-2">This is the home page:</span>
+		  <span class="text-h5 mb-2">Please choose a severity level:</span>
 		  <br />
 		</div>
 		<CardText>
 		  Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, qui quaerat
 		  rerum incidunt nisi ducimus?
 		</CardText>
+		<CardActions>
+				<Button block class="red white-text">High</Button>
+		</CardActions>
+		<CardActions>
+			<Button block class="yellow white-text">Medium</Button>
+		</CardActions>
+		<CardActions>
+			<Button block class="green white-text">Low</Button>
+		</CardActions>
 	  </Card>
 	
 </MaterialApp>
