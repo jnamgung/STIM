44a8d36037c0226a9a006b1530885e8e56c92d57
Jas Haria <37465756+jas-haria@users.noreply.github.com>
2022-04-18 19:03:26 -0400

    Merging frontend for actions and symptoms

--- app-frontend/src/lib/header/Header.svelte
+++ app-frontend/src/lib/header/Header.svelte
@@ -22,6 +22,12 @@
 			<li class:active={$page.url.pathname === '/another_page'}>
 				<a sveltekit:prefetch href="/another_page">Another Page</a>
 			</li>
+			<li class:active={$page.url.pathname === '/actions'}>
+				<a sveltekit:prefetch href="/actions">Actions</a>
+			</li>
+			<li class:active={$page.url.pathname === '/symptoms'}>
+				<a sveltekit:prefetch href="/symptoms">Symptoms</a>
+			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
